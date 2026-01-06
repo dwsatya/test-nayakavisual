8f8a42f54f2df8e8ad36beec90f01a2ed7e0faee
dwsatya <satyaputra467@gmail.com>
2026-01-07 01:33:15 +0800

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { Routes, Route } from "react-router-dom";
-import Client1 from "./pages/client1/index.jsx";
-import Client2 from "./pages/client2/index.jsx";
-import Login from "./pages/Login/index.jsx";
+import { Routes, Route, Link } from "react-router-dom";
+import Client1 from "./pages/client1";
+import Client2 from "./pages/client2";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
     <div className="App">
+      {/* NAVBAR */}
+      <nav style={{ padding: 16 }}>
+        <Link to="/" style={{ marginRight: 12 }}>Client 1</Link>
+        <Link to="/client2">Client 2</Link>
+      </nav>
+
+      {/* ROUTES */}
       <Routes>
         <Route path="/" element={<Client1 />} />
         <Route path="/client2" element={<Client2 />} />
